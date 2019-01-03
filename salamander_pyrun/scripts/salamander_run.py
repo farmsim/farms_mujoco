1f436e7a42fdb06aa2cf8993e14c69324a094554
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-01-04 00:13:16 +0100

    salamander_pyrun: Using sqlite3 to unleash true parallel simulations

--- salamander_pyrun/scripts/salamander_run.py
+++ salamander_pyrun/scripts/salamander_run.py
@@ -2,9 +2,9 @@
 """ Test salamander run """
 
 import argparse
+# import threading
+import multiprocessing
 import salamander_pyrun as sr
-world_path = "/.gazebo/models/{}/world.world".format(model_name)
-sr.run_simulation(world_path)
 
 
 def run(model_name):
@@ -15,23 +15,29 @@ def run(model_name):
 
 def parse_args():
     """ Parse arguments """
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Model names')
     parser.add_argument(
-        'model_name',
+        'model_names',
         type=str,
         nargs='+',
         help='Name of Gazebo model to simulate'
     )
     args = parser.parse_args()
-    print(args.model_name)
-    return args.model_name
+    print(args.model_names)
+    return args.model_names
 
 
 def main():
     """ Main """
     model_names = parse_args()
-    for model_name in model_names:
-        run(model_name)
+    processes = [
+        multiprocessing.Process(target=run, args=(model_name, ))
+        for model_name in model_names
+    ]
+    for _p in processes:
+        _p.start()
+    for _p in processes:
+        _p.join()
 
 
 if __name__ == '__main__':
