22aad98a0595ca616767eee0ab9fdaaf0f00758e
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-01-05 01:54:24 +0100

    Salamander_generation: Added new parameters to control in generation

--- salamander_generation/salamander_generation/gen_controller.py
+++ salamander_generation/salamander_generation/gen_controller.py
@@ -182,34 +182,41 @@ class ControlJoint(OrderedDict):
 class ControlJoints(OrderedDict):
     """ ControlJoints """
 
-    def __init__(self, gait, frequency, body_bias, **kwargs):
+    def __init__(self, gait, frequency, **kwargs):
         super(ControlJoints, self).__init__()
+        walking_gait = (gait == "walking")
         # Body
         n_body = kwargs.pop("n_body", 11)
         n_legs = kwargs.pop("n_legs", 2)
+        body_amplitude = kwargs.pop("body_amplitude", 0.3)
+        body_bias = kwargs.pop("body_bias", 0)
         for i in range(n_body):
-            amplitude = 0.3 if gait == "walking" else 0.1+i*0.4/n_body
+            amplitude = (
+                body_amplitude
+                if walking_gait
+                else 0.1+i*0.4/n_body
+            )
             self["link_body_{}".format(i+1)] = ControlJoint(
                 type="position",
                 oscillator=ControlOscillator(
                     amplitude=(
                         float(amplitude*np.sin(2*np.pi*i/n_body))
-                        if gait == "walking"
+                        if walking_gait
                         else float(amplitude)
                     ),
                     frequency=frequency,
-                    phase=0 if gait == "walking" else float(2*np.pi*i/n_body),
+                    phase=0 if walking_gait else float(2*np.pi*i/n_body),
                     bias=body_bias
                 ),
                 pid=ControlPIDs(
                     position=ControlPID(
-                        p=1e1 if gait == "walking" else 1e1,
-                        i=1e0 if gait == "walking" else 0,
+                        p=1e1 if walking_gait else 1e1,
+                        i=1e0 if walking_gait else 0,
                         d=0
                     ),
                     velocity=ControlPID(
-                        p=1e-2 if gait == "walking" else 1e-2,
-                        i=1e-3 if gait == "walking" else 0,
+                        p=1e-2 if walking_gait else 1e-2,
+                        i=1e-3 if walking_gait else 0,
                         d=0
                     )
                 )
@@ -228,12 +235,12 @@ class ControlJoints(OrderedDict):
                         oscillator=ControlOscillator(
                             amplitude=(
                                 float(0.6 if part_i == 0 else 0.1)
-                                if gait == "walking"
+                                if walking_gait
                                 else 0.0
                             ),
                             frequency=(
                                 float(frequency)
-                                if gait == "walking"
+                                if walking_gait
                                 else 0
                             ),
                             phase=(
@@ -241,12 +248,12 @@ class ControlJoints(OrderedDict):
                                     np.pi*np.abs(leg_i-side_i)
                                     + (0 if part_i == 0 else 0.5*np.pi)
                                 )
-                                if gait == "walking"
+                                if walking_gait
                                 else 0
                             ),
                             bias=(
                                 float(0 if part_i == 0 else 0.1)
-                                if gait == "walking"
+                                if walking_gait
                                 else -2*np.pi/5 if part_i == 0
                                 else 0
                             )
@@ -282,17 +289,12 @@ class ControlParameters(OrderedDict):
         super(ControlParameters, self).__init__()
         self.gait = gait
         self.frequency = frequency
-        self.body_bias = kwargs.pop("body_bias", 0)
-        self.n_body = kwargs.pop("n_body", 11)
-        self.n_legs = kwargs.pop("n_legs", 2)
         self["joints"] = kwargs.pop(
             "joints",
             ControlJoints(
-                self.gait,
-                self.frequency,
-                self.body_bias,
-                n_body=self.n_body,
-                n_legs=self.n_legs
+                gait=self.gait,
+                frequency=self.frequency,
+                **kwargs
             )
         )
 
