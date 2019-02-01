9ac0039f0bdabf9c8606158baefa104747b4edea
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-02-01 20:09:31 +0100

    Salamander_evolution: Preparing problem benchmarking

--- salamander_evolution/scripts/salamander_test_parallel_evolution.py
+++ salamander_evolution/scripts/salamander_test_parallel_evolution.py
@@ -7,15 +7,23 @@ from multiprocessing import Pool
 import pygmo as pg
 import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
 
 class SphereFunction:
     """SphereFunction"""
 
     @staticmethod
+    def fitness_function_fast(decision_vector):
+        """Fitnesss"""
+        return [np.linalg.norm(decision_vector - SphereFunction.best_known())]
+
+    @staticmethod
     def fitness_function(decision_vector):
         """Fitnesss"""
         time.sleep(0.5)
-        return [sum(decision_vector*decision_vector)]
+        return SphereFunction.fitness_function_fast(decision_vector)
 
     def fitness(self, decision_vector):
         """Fitnesss"""
@@ -26,6 +34,11 @@ class SphereFunction:
     def get_bounds():
         """Get bounds"""
         return ([-1, -1], [1, 1])
+
+    @staticmethod
+    def best_known():
+        """Best known"""
+        return np.array([1, 2])
 
 
 def run_archipelago():
@@ -101,6 +114,59 @@ class JonAlgorithm:
         return pop
 
 
+def plot_fitness(problem, distribution, best=None, figure=None):
+    """Plot fitess landscape"""
+    # x_distribution, y_distribution = (
+    #     [
+    #         np.arange(-3.0, 3.0, delta),
+    #         np.arange(-3.0, 3.0, delta)
+    #     ] if distribution is None else distribution
+    # )
+    # _x, _y = np.meshgrid(distribution[0], distribution[1])
+    # _z = np.array([problem(np.array([_i, _j])) for _i, _j in zip(_x, _y)])
+    if figure is not None:
+        plt.figure(figure)
+    _z = np.array([
+        [
+            problem(np.array([_x, _y]))[0]
+            for _y in distribution[1]
+        ] for _x in distribution[0]
+    ])
+    plt.imshow(
+        _z.transpose(), interpolation='bicubic',  # bicubic
+        cmap=cm.RdYlGn,
+        origin='lower',
+        extent=[
+            min(distribution[0]), max(distribution[0]),
+            min(distribution[1]), max(distribution[1])
+        ],
+        # vmax=abs(_z).max(), vmin=-abs(_z).max()
+    )
+    plt.colorbar()
+    if best is not None:
+        plt.plot(best[0], best[1], "b*", markersize=20)
+
+    # ln, = plt.plot([], [], 'ro', animated=True)
+
+    # def init():
+    #     ax.set_xlim(0, 2*np.pi)
+    #     ax.set_ylim(-1, 1)
+    #     return ln,
+
+    # def update(frame):
+    #     xdata.append(frame)
+    #     ydata.append(np.sin(frame))
+    #     ln.set_data(xdata, ydata)
+    #     return ln,
+
+    # return FuncAnimation(
+    #     fig, update,
+    #     frames=np.linspace(0, 2*np.pi, 128),
+    #     init_func=init, blit=True
+    # )
+
+
+
 def main():
     """Main"""
     # Problem
@@ -131,4 +197,30 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    for problem in [
+            pg.ackley(dim=2),
+            pg.griewank(dim=2),
+            # pg.hock_schittkowsky_71(dim=2),
+            # pg.inventory(dim=2),
+            # pg.luksan_vlcek1(dim=2),
+            pg.rastrigin(dim=2),
+            # pg.minlp_rastrigin(dim=2),
+            pg.rosenbrock(dim=2),
+            pg.schwefel(dim=2)
+    ]:
+        _problem = pg.problem(problem)
+        print("Running problem: {}".format(_problem.get_name()))
+        bounds_min, bounds_max = _problem.get_bounds()
+        algo = pg.algorithm(pg.cmaes(gen=10, force_bounds=True))
+        pop = pg.population(_problem, size=100)
+        pop = algo.evolve(pop)
+        plot_fitness(
+            _problem.fitness,
+            distribution=[
+                np.linspace(bounds_min[0], bounds_max[0], 300),
+                np.linspace(bounds_min[1], bounds_max[1], 300)
+            ],
+            best=problem.best_known(),
+            figure=_problem.get_name()
+        )
+    plt.show()
