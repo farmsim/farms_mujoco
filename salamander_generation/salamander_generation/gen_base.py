076573e9ee65046cfa0a705fb29d5515c5cb2e4a
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-02-20 15:08:51 +0100

    Salamander_generation: Moved sdf related functions to sdf_utils.py

--- salamander_generation/salamander_generation/gen_base.py
+++ salamander_generation/salamander_generation/gen_base.py
@@ -3,13 +3,13 @@
 
 import os
 
-import xml.dom.minidom
 from xml.etree import ElementTree as etree
 
 from .model import (
     generate_model_options,
     ModelGenerationTemplates
 )
+from .sdf_utils import new_sdf_text
 
 
 class FrictionParameters:
@@ -212,26 +212,6 @@ def correct_sdf_visuals_materials(root):
         for stuff in visual.findall('material'):
             visual.remove(stuff)
     return root
-
-
-def new_sdf_text(root):
-    """Write new SDF file"""
-    xml_string = etree.tostring(root).decode()
-    xml_string = xml_string.replace("\n", "")
-    while "  " in xml_string:
-        xml_string = xml_string.replace("  ", "")
-    xml_text = xml.dom.minidom.parseString(xml_string)
-    return str(xml_text.toprettyxml(
-        indent="  ",
-        newl='\n'
-        # encoding="UTF-8"
-    ))
-
-
-def write_new_sdf(root, path_sdf):
-    """Write new SDF file (Deprecated)"""
-    with open(path_sdf, "w+") as sdf_file:
-        sdf_file.write(new_sdf_text(root))
 
 
 def create_new_model(previous_model, new_model, friction):
