322aaf8ef99caf7af77652e499e858154ab30c94
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-02-25 14:29:40 +0100

    Salamander_evolution: Implemented nsine fitting script

--- salamander_evolution/scripts/salamander_evolution_test_phase.py
+++ salamander_evolution/scripts/salamander_evolution_test_phase.py
@@ -18,6 +18,11 @@ class SineModel:
         self.phase = phase
         self.offset = offset
 
+    @classmethod
+    def from_vector(cls, vector):
+        """From vector"""
+        return cls(*vector)
+
     def to_vector(self):
         """To vector"""
         return np.array([
@@ -27,20 +32,51 @@ class SineModel:
             self.offset
         ])
 
-    @classmethod
-    def from_vector(cls, vector):
-        """From vector"""
-        return cls(*vector)
-
     def data(self, xdata):
         """Data"""
         return (
             self.amplitude*np.sin(
                 2*np.pi*self.frequency*xdata
                 + self.phase
-            )
-            + self.offset
+            ) + self.offset
         )
+
+    def plot(self, label, start, end, style="-"):
+        """Plot"""
+        xdata = np.linspace(start, end, 3e3)
+        plt.plot(xdata, self.data(xdata), style, label=label)
+        plt.xlabel("Time [s]")
+        plt.ylabel("Data")
+        plt.legend()
+        plt.grid(True)
+
+
+class NSineModel:
+    """SineModel"""
+
+    def __init__(self, sines):
+        super(NSineModel, self).__init__()
+        self.sines = sines
+
+    @classmethod
+    def from_vector(cls, vector):
+        """From vector"""
+        assert len(vector) % 4 == 0
+        return cls([
+            SineModel.from_vector(vector[4*i:4*i+4])
+            for i in range(len(vector)//4)
+        ])
+
+    def to_vector(self):
+        """To vector"""
+        return np.concatenate([
+            sine.to_vector()
+            for sine in self.sines
+        ])
+
+    def data(self, xdata):
+        """Data"""
+        return np.sum([sine.data(xdata) for sine in self.sines], axis=0)
 
     def plot(self, label, start, end, style="-"):
         """Plot"""
@@ -67,7 +103,7 @@ class SineFitting:
         self._max_freq = 0.5*len(xdata)/(xdata[-1] - xdata[0])
 
     def fitness(self, decision_vector):
-        """Fitnesss"""
+        """Fitness"""
         fit_sine = SineModel.from_vector(decision_vector)
         fit_data = fit_sine.data(self.xdata)
         return [np.mean(np.square(self._data - fit_data))]
@@ -89,25 +125,71 @@ class SineFitting:
         return self._sine.to_vector()
 
 
+class NSineFitting:
+    """NSineFitting"""
+
+    def __init__(self, nsines, xdata):
+        super(NSineFitting, self).__init__()
+        self._nsines = nsines
+        self._size = len(nsines.sines)
+        self.xdata = xdata
+        self._data = (
+            nsines.data(xdata)
+            # + np.random.normal(0, 0.1*self._nsines.amplitude, np.size(xdata))
+        )
+        self._name = "Nsines fitting"
+        self._max_freq = 0.5*len(xdata)/(xdata[-1] - xdata[0])
+
+    def fitness(self, decision_vector):
+        """Fitness"""
+        fit_nsines = NSineModel.from_vector(decision_vector)
+        fit_data = fit_nsines.data(self.xdata)
+        return [np.mean(np.square(self._data - fit_data))]
+        # + 1e-3*(fit_nsines.frequency**2)
+
+    def get_name(self):
+        """Get name"""
+        return self._name
+
+    def get_bounds(self):
+        """Get bounds"""
+        return (
+            np.concatenate([[0, 0, 0, -100] for _ in range(self._size)]),
+            np.concatenate([
+                [100, self._max_freq, 2*np.pi, 100]
+                for _ in range(self._size)
+            ])
+        )
+
+    def best_known(self):
+        """Best known"""
+        return self._nsines.to_vector()
+
+
 def main():
     """Main"""
+
+    problem_size = 2
+
     # Original data
-    amplitude = 10*np.random.ranf()
-    freq = 10*np.random.ranf()
-    phase = 2*np.pi*np.random.ranf()
-    offset = 10*np.random.ranf()
-    model = SineModel(amplitude, freq, phase, offset)
+    amplitudes = [10*np.random.ranf() for _ in range(problem_size)]
+    freqs = [10*np.random.ranf() for _ in range(problem_size)]
+    phases = [2*np.pi*np.random.ranf() for _ in range(problem_size)]
+    offsets = [10*np.random.ranf() for _ in range(problem_size)]
+    models = [
+        SineModel(amplitudes[i], freqs[i], phases[i], offsets[i])
+        for i in range(problem_size)
+    ]
+    nmodels = NSineModel(models)
     xdata = np.linspace(0, 3, 100)
 
     # Model
-    # amplitude*sin(2*np.pi*frequency + phase)
-    # Parameters: amplitude, frequency, phase
     kwargs = {"memory": True, "seed": 0}
-    n_threads = 10
+    n_threads = 4
     algorithms = [pg.sade(gen=1, **kwargs) for _ in range(n_threads)]
 
     # Optimisation problem
-    problem = SineFitting(model, xdata)
+    problem = NSineFitting(nmodels, xdata)
 
     # Evolution
     print("Running evolution")
@@ -115,28 +197,30 @@ def main():
         problem=problem,
         algorithms=algorithms,
         n_pop=10,
-        n_gen=100
+        n_gen=300
     )
 
     # Result
     print("Evolution complete, getting result")
     champion = evolution.champion()
-    fit_model = SineModel.from_vector(champion[0])
-    message = (
-        "Errors (fitness={}):"
-        "\n  Amplitude: {}"
-        "\n  Frequency: {}"
-        "\n  Phase: {}"
-        "\n  Offset: {}"
-    )
-    print(message.format(
-        champion[1],
-        np.abs(model.amplitude - fit_model.amplitude),
-        np.abs(model.frequency - fit_model.frequency),
-        np.abs(model.phase - fit_model.phase) % (2*np.pi),
-        np.abs(model.offset - fit_model.offset)
-    ))
-    model.plot("Original data", xdata[0], xdata[-1], ".-")
+    fit_model = NSineModel.from_vector(champion[0])
+    # champion = evolution.champion()
+    # fit_model = SineModel.from_vector(champion[0])
+    # message = (
+    #     "Errors (fitness={}):"
+    #     "\n  Amplitude: {} %"
+    #     "\n  Frequency: {} %"
+    #     "\n  Phase: {}"
+    #     "\n  Offset: {}"
+    # )
+    # print(message.format(
+    #     champion[1],
+    #     100*np.abs((fit_model.amplitude - model.amplitude) / model.amplitude),
+    #     100*np.abs((fit_model.frequency - model.frequency) / model.frequency),
+    #     np.abs(model.phase - fit_model.phase) % (2*np.pi),
+    #     np.abs(model.offset - fit_model.offset)
+    # ))
+    nmodels.plot("Original data", xdata[0], xdata[-1], ".-")
     fit_model.plot("Fitted data", xdata[0], xdata[-1], "-")
     plt.show()
 
