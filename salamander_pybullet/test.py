9b18171ddeb22eb20d7497667e82c5a3e6ebe722
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-03-11 14:15:33 +0100

    Salamander_pybullet: Removed tiny_renderer from render options

--- salamander_pybullet/test.py
+++ salamander_pybullet/test.py
@@ -1554,7 +1554,7 @@ def rendering(render=1):
     """Enable/disable rendering"""
     pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, render)
     pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, render)
-    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_TINY_RENDERER, render)
+    # pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_TINY_RENDERER, render)
 
 
 class ExperimentLogger:
