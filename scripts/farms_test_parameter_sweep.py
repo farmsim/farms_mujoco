fe81e11fe2cd58447f4db9f9d48cb387ca8fc007
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-03-26 08:55:24 +0100

    Added plot to parameter sweep to visualise influence of frequency

--- scripts/farms_test_parameter_sweep.py
+++ scripts/farms_test_parameter_sweep.py
@@ -5,16 +5,33 @@ from multiprocessing import Pool
 from farms_bullet.simulation import Simulation
 from farms_bullet.simulation_options import SimulationOptions
 import numpy as np
+from scipy import interpolate
+import matplotlib.pyplot as plt
 
 
 def run_simulation(options):
     """Run simulation"""
     sim = Simulation(options=options)
     sim.run(options)
+    return [
+        sim.frequency,
+        sim.body_amplitude,
+        np.linalg.norm(
+            sim.experiment_logger.positions.data[:-1]
+            - sim.experiment_logger.positions.data[0]
+        )
+    ]
 
 
 def main():
     """Main"""
+    frequencies = np.linspace(0, 3, 10)
+    body_amplitudes = np.linspace(0, 0.3, 5)
+    parameters = np.array([
+        [f, b]
+        for f in frequencies
+        for b in body_amplitudes
+    ])
     simulations_options = [
         SimulationOptions.with_clargs(
             headless=True,
@@ -23,12 +40,29 @@ def main():
             frequency=frequency,
             body_amplitude=body_amplitude
         )
-        for frequency in np.linspace(0.5, 2, 8)
-        for body_amplitude in np.linspace(0.1, 0.3, 5)
+        for frequency in frequencies
+        for body_amplitude in body_amplitudes
     ]
     pool = Pool(4)
-    pool.map(run_simulation, simulations_options)
+    results = np.array(pool.map(run_simulation, simulations_options))
     print("DONE: Completed parameter sweep")
+    print(results)
+    results_interp = interpolate.interp2d(
+        results[:, 0], results[:, 1], results[:, 2],
+        kind='linear'  # cubic
+    )
+    xnew = np.linspace(min(frequencies), max(frequencies), 100)
+    ynew = np.linspace(min(body_amplitudes), max(body_amplitudes), 100)
+    znew = results_interp(xnew, ynew)
+
+    extent = (min(xnew), max(xnew), min(ynew), max(ynew))
+    plt.plot(parameters[:, 0], parameters[:, 1], "rx")
+    plt.imshow(znew, extent=extent, origin='lower', aspect='auto')
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Body amplitude [rad]")
+    cbar = plt.colorbar()
+    cbar.set_label('Distance [m]')
+    plt.show()
 
 
 if __name__ == '__main__':
