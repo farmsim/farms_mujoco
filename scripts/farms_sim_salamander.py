641ac8dd2047c793ec504b37db90e47bfd49e88c
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-03-26 15:43:16 +0100

    Added model options to basic simulation script

--- scripts/farms_sim_salamander.py
+++ scripts/farms_sim_salamander.py
@@ -2,10 +2,18 @@
 """Run salamander simulation with bullet"""
 
 from farms_bullet.simulation import main as run_simulation
+from farms_bullet.model_options import ModelOptions
+
 
 def main():
     """Main"""
-    run_simulation()
+    model_options = ModelOptions()
+    # model_options = ModelOptions(
+    #     frequency=1.7,
+    #     body_stand_amplitude=0.42
+    # )
+    run_simulation(model_options=model_options)
+
 
 if __name__ == '__main__':
     main()
