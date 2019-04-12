f3e78654e4c7bd5e64edd17f6ee02a7f98a6fd16
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-04-12 15:04:03 +0200

    INTERESTING: Implemented fast sparse ODE solver

--- src/controller.pyx
+++ src/controller.pyx
@@ -24,7 +24,14 @@ DTYPE = np.float64
 @cython.wraparound(False)   # Deactivate negative indexing.
 @cython.profile(False)
 @cython.nonecheck(False)
-cpdef void odefun(CTYPE[:] dstate, CTYPE[:] state, CTYPE[:] freqs, CTYPE[:, :] weights, CTYPE[:, :] phi, unsigned int n_dim) nogil:
+cpdef void odefun(
+    CTYPE[:] dstate,
+    CTYPE[:] state,
+    CTYPE[:] freqs,
+    CTYPE[:, :] weights,
+    CTYPE[:, :] phi,
+    unsigned int n_dim
+) nogil:
     """ODE"""
     cdef int i, j, n_dim_c = n_dim
     for i in prange(n_dim_c, nogil=True):
@@ -39,7 +46,15 @@ cpdef void odefun(CTYPE[:] dstate, CTYPE[:] state, CTYPE[:] freqs, CTYPE[:, :] w
 @cython.wraparound(False)   # Deactivate negative indexing.
 @cython.profile(False)
 @cython.nonecheck(False)
-cpdef void rk4_ode(fun, float timestep, CTYPE[:] state, CTYPE[:] freqs, CTYPE[:, :] weights, CTYPE[:, :] phi, unsigned int n_dim):
+cpdef void rk4_ode(
+    fun,
+    float timestep,
+    CTYPE[:] state,
+    CTYPE[:] freqs,
+    CTYPE[:, :] weights,
+    CTYPE[:, :] phi,
+    unsigned int n_dim
+):
     """Runge-Kutta step integration"""
     cdef int i, n_dim_c = n_dim
     cdef CTYPE[:] k_1 = np.empty([n_dim], dtype=DTYPE)
@@ -62,6 +77,72 @@ cpdef void rk4_ode(fun, float timestep, CTYPE[:] state, CTYPE[:] freqs, CTYPE[:,
         k_3[i] = timestep*k_3[i]
         k_3_2[i] = state[i]+k_3[i]
     fun(k_4, k_3_2, freqs, weights, phi, n_dim_c)
+    for i in prange(n_dim_c, nogil=True):
+        k_4[i] = timestep*k_4[i]
+        state[i] = state[i] + (k_1[i]+2*k_2[i]+2*k_3[i]+k_4[i])/6.
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+@cython.profile(False)
+@cython.nonecheck(False)
+cpdef void odefun_sparse(
+    CTYPE[:] dstate,
+    CTYPE[:] state,
+    CTYPE[:] freqs,
+    unsigned int[:, :] connectivity,
+    CTYPE[:, :] connection,
+    unsigned int n_dim,
+    unsigned int c_dim
+) nogil:
+    """ODE"""
+    cdef int i, j, n_dim_c = n_dim, c_dim_c = c_dim
+    for i in prange(n_dim_c, nogil=True):
+        dstate[i] = freqs[i]
+    for j in range(c_dim_c):
+        dstate[connectivity[j][0]] += connection[j][0]*sin(
+            state[connectivity[j][1]]
+            - state[connectivity[j][0]]
+            + connection[j][1]
+        )
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+@cython.profile(False)
+@cython.nonecheck(False)
+cpdef void rk4_ode_sparse(
+    fun,
+    float timestep,
+    CTYPE[:] state,
+    CTYPE[:] freqs,
+    unsigned int[:, :] connectivity,
+    CTYPE[:, :] connection,
+    unsigned int n_dim,
+    unsigned int c_dim
+):
+    """Runge-Kutta step integration"""
+    cdef int i, n_dim_c = n_dim, c_dim_c = c_dim
+    cdef CTYPE[:] k_1 = np.empty([n_dim], dtype=DTYPE)
+    cdef CTYPE[:] k_2 = np.empty([n_dim], dtype=DTYPE)
+    cdef CTYPE[:] k_3 = np.empty([n_dim], dtype=DTYPE)
+    cdef CTYPE[:] k_4 = np.empty([n_dim], dtype=DTYPE)
+    cdef CTYPE[:] k_1_2 = np.empty([n_dim], dtype=DTYPE)
+    cdef CTYPE[:] k_2_2 = np.empty([n_dim], dtype=DTYPE)
+    cdef CTYPE[:] k_3_2 = np.empty([n_dim], dtype=DTYPE)
+    fun(k_1, state, freqs, connectivity, connection, n_dim_c, c_dim_c)
+    for i in prange(n_dim_c, nogil=True):
+        k_1[i] = timestep*k_1[i]
+        k_1_2[i] = state[i]+0.5*k_1[i]
+    fun(k_2, k_1_2, freqs, connectivity, connection, n_dim_c, c_dim_c)
+    for i in prange(n_dim_c, nogil=True):
+        k_2[i] = timestep*k_2[i]
+        k_2_2[i] = state[i]+0.5*k_2[i]
+    fun(k_3, k_2_2, freqs, connectivity, connection, n_dim_c, c_dim_c)
+    for i in prange(n_dim_c, nogil=True):
+        k_3[i] = timestep*k_3[i]
+        k_3_2[i] = state[i]+k_3[i]
+    fun(k_4, k_3_2, freqs, connectivity, connection, n_dim_c, c_dim_c)
     for i in prange(n_dim_c, nogil=True):
         k_4[i] = timestep*k_4[i]
         state[i] = state[i] + (k_1[i]+2*k_2[i]+2*k_3[i]+k_4[i])/6.
