f7ca6c7761a8ac6859fa5ea7abd2fac3751ce2f7
Blaise Arthur Etter <blaise.etter@epfl.ch>
2019-05-14 15:05:56 +0200

    adding the second file for model option

--- farms_bullet/controllers/network.py
+++ farms_bullet/controllers/network.py
@@ -180,42 +180,33 @@ class SalamanderNetworkParameters(ODE):
         self[1][0] = OscillatorArray.for_moving()
         self[1][1] = ConnectivityArray.for_moving()
         self[1][2] = JointsArray.for_moving()
-
-        #if gait == "walking":
-        #    self[1][0] = OscillatorArray.for_walking()
-        #    self[1][1] = ConnectivityArray.for_walking()
-        #    self[1][2] = JointsArray.for_walking()
-        #else:
-        #    self[1][0] = OscillatorArray.for_swimming()
-        #    self[1][1] = ConnectivityArray.for_swimming()
-        #    self[1][2] = JointsArray.for_swimming()
-
+        
     @staticmethod
     def my_network():
         """implementation of blaise"""    
         oscillators = OscillatorArray.for_moving()
         connectivity = ConnectivityArray.for_moving()
         joints = JointsArray.for_moving()
-        pdb.set_trace()
+        #pdb.set_trace()
         return oscillators, connectivity, joints
 
-    @staticmethod
-    def walking_parameters():
-        """Walking parameters"""
-        raise Exception
-        oscillators = OscillatorArray.for_walking()
-        connectivity = ConnectivityArray.for_walking()
-        joints = JointsArray.for_walking()
-        return oscillators, connectivity, joints
+    # @staticmethod
+    # def walking_parameters():
+    #     """Walking parameters"""
+    #     raise Exception
+    #     oscillators = OscillatorArray.for_walking()
+    #     connectivity = ConnectivityArray.for_walking()
+    #     joints = JointsArray.for_walking()
+    #     return oscillators, connectivity, joints
 
-    @staticmethod
-    def swimming_parameters():
-        """Swimming parameters"""
-        raise Exception
-        oscillators = OscillatorArray.for_swimming()
-        connectivity = ConnectivityArray.for_swimming()
-        joints = JointsArray.for_swimming()
-        return oscillators, connectivity, joints
+    # @staticmethod
+    # def swimming_parameters():
+    #     """Swimming parameters"""
+    #     raise Exception
+    #     oscillators = OscillatorArray.for_swimming()
+    #     connectivity = ConnectivityArray.for_swimming()
+    #     joints = JointsArray.for_swimming()
+    #     return oscillators, connectivity, joints
 
     @classmethod
     def for_walking(cls):
@@ -303,70 +294,70 @@ class  OscillatorArray(NetworkArray):
             
         return freqs, rates, amplitudes
 
-    @staticmethod
-    def walking_parameters():
-        """Walking parameters"""
-        raise Exception
+    # @staticmethod
+    # def walking_parameters():
+    #     """Walking parameters"""
+    #     raise Exception
 
-        opt_mod = ModelOptions()
-        n_body = opt_mod['n_body']
-        n_dof_legs = opt_mod['n_dof_legs']
-        n_legs = opt_mod['n_legs']
-        n_joints = n_body + n_legs * n_dof_legs
-        n_oscillators = 2 * (n_joints)
-        freqs = 2 * np.pi * np.ones(n_oscillators)
-        rates = 10 * np.ones(n_oscillators)
-        options = SalamanderControlOptions.walking()
-        # Amplitudes
-        amplitudes = np.zeros(n_oscillators)
-        for i in range(n_body):
-            amplitudes[[i, i + n_body]] = np.abs(
-                options["body_stand_amplitude"] * np.sin(
-                    2 * np.pi * i / n_body
-                    - options["body_stand_shift"]
-                )
-            )
-        for leg_i in range(n_legs):
-            for i in range(n_dof_legs):
-                amplitudes[[
-                    2 * n_body + 2 * leg_i * n_dof_legs + i,
-                    2 * n_body + 2 * leg_i * n_dof_legs + i + n_dof_legs
-                ]] = np.abs(
-                    options["leg_{}_amplitude".format(i)]
-                )
-        return freqs, rates, amplitudes
+    #     opt_mod = ModelOptions()
+    #     n_body = opt_mod['n_body']
+    #     n_dof_legs = opt_mod['n_dof_legs']
+    #     n_legs = opt_mod['n_legs']
+    #     n_joints = n_body + n_legs * n_dof_legs
+    #     n_oscillators = 2 * (n_joints)
+    #     freqs = 2 * np.pi * np.ones(n_oscillators)
+    #     rates = 10 * np.ones(n_oscillators)
+    #     options = SalamanderControlOptions.walking()
+    #     # Amplitudes
+    #     amplitudes = np.zeros(n_oscillators)
+    #     for i in range(n_body):
+    #         amplitudes[[i, i + n_body]] = np.abs(
+    #             options["body_stand_amplitude"] * np.sin(
+    #                 2 * np.pi * i / n_body
+    #                 - options["body_stand_shift"]
+    #             )
+    #         )
+    #     for leg_i in range(n_legs):
+    #         for i in range(n_dof_legs):
+    #             amplitudes[[
+    #                 2 * n_body + 2 * leg_i * n_dof_legs + i,
+    #                 2 * n_body + 2 * leg_i * n_dof_legs + i + n_dof_legs
+    #             ]] = np.abs(
+    #                 options["leg_{}_amplitude".format(i)]
+    #             )
+    #     return freqs, rates, amplitudes
 
-    @staticmethod
-    def swimming_parameters():
-        """Swimming parameters"""
-        raise Exception
+    # @staticmethod
+    # def swimming_parameters():
+    #     """Swimming parameters"""
+    #     raise Exception
 
-        opt_mod = ModelOptions()
-        n_body = opt_mod['n_body']
-        n_dof_legs = opt_mod['n_dof_legs']
-        n_legs = opt_mod['n_legs']
-        n_joints = n_body + n_legs * n_dof_legs
-        n_oscillators = 2 * (n_joints)
-        freqs = 2 * np.pi * np.ones(n_oscillators)
-        rates = 10 * np.ones(n_oscillators)
-        amplitudes = np.zeros(n_oscillators)
-        options = SalamanderControlOptions.swimming()
-        body_amplitudes = np.linspace(
-            options["body_amplitude_0"],
-            options["body_amplitude_1"],
-            n_body
-        )
-        for i in range(n_body):
-            amplitudes[[i, i + n_body]] = body_amplitudes[i]
-        for leg_i in range(n_legs):
-            for i in range(n_dof_legs):
-                amplitudes[[
-                    2 * n_body + 2 * leg_i * n_dof_legs + i,
-                    2 * n_body + 2 * leg_i * n_dof_legs + i + n_dof_legs
-                ]] = (
-                    options["leg_{}_amplitude".format(i)]
-                )
-        return freqs, rates, amplitudes
+    #     opt_mod = ModelOptions()
+    #     n_body = opt_mod['n_body']
+    #     n_dof_legs = opt_mod['n_dof_legs']
+    #     n_legs = opt_mod['n_legs']
+    #     n_joints = n_body + n_legs * n_dof_legs
+    #     n_oscillators = 2 * (n_joints)
+    #     freqs = 2 * np.pi * np.ones(n_oscillators)
+    #     rates = 10 * np.ones(n_oscillators)
+    #     amplitudes = np.zeros(n_oscillators)
+    #     options = SalamanderControlOptions.swimming()
+    #     body_amplitudes = np.linspace(
+    #         options["body_amplitude_0"],
+    #         options["body_amplitude_1"],
+    #         n_body
+    #     )
+    #     for i in range(n_body):
+    #         amplitudes[[i, i + n_body]] = body_amplitudes[i]
+    #     for leg_i in range(n_legs):
+    #         for i in range(n_dof_legs):
+    #             amplitudes[[
+    #                 2 * n_body + 2 * leg_i * n_dof_legs + i,
+    #                 2 * n_body + 2 * leg_i * n_dof_legs + i + n_dof_legs
+    #             ]] = (
+    #                 options["leg_{}_amplitude".format(i)]
+    #             )
+    #     return freqs, rates, amplitudes
 
     @classmethod
     def for_moving(cls):
