3cc63a8b1ba88e59f544d7d11e39cce31fa95fd2
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-06-03 17:20:52 +0200

    Corrected intrinsic frequencies characteristics (Thanks Blaise)

--- farms_bullet/experiments/salamander/animat_options.py
+++ farms_bullet/experiments/salamander/animat_options.py
@@ -208,6 +208,7 @@ class SalamanderOscillatorFrequenciesOptions(DriveDependentProperty):
         """Legs intrinsic frequencies"""
         return  cls([
             [0, 0],
+            [1, 0],
             [1, 0.5],
             [3, 1.5],
             [3, 0],
@@ -220,6 +221,7 @@ class SalamanderOscillatorFrequenciesOptions(DriveDependentProperty):
         return cls([
             [0, 0],
             [1, 0],
+            [1, 1.5],
             [5, 4],
             [5, 0],
             [6, 0]
