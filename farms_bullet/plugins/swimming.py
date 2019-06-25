5cb68a5d05d563309974d091ef153aa84e6aea56
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-06-25 20:36:43 +0200

    Added position plots for debugging SPH coupling

--- farms_bullet/plugins/swimming.py
+++ farms_bullet/plugins/swimming.py
@@ -58,3 +58,46 @@ def viscous_swimming(
             ),
             flags=pybullet.LINK_FRAME
         )
+        # if link_i == 11:
+            # dynamics = pybullet.getDynamicsInfo(model, -1)
+            # pos = np.array(dynamics[3])
+            # base = data_gps.com_position(iteration, link_i) + np.dot(
+            #     np.array(
+            #         pybullet.getMatrixFromQuaternion(ori)
+            #     ).reshape([3, 3]),
+            #     -pos
+            # )
+        joint = np.array(data_gps.urdf_position(iteration, link_i))
+        if link_i == 11:
+            print("RBP position: {}".format(np.array(joint)))
+        joint_ori = np.array(data_gps.urdf_orientation(iteration, link_i))
+        com_ori = np.array(data_gps.com_orientation(iteration, link_i))
+        ori_joint = np.array(
+            pybullet.getMatrixFromQuaternion(joint_ori)
+        ).reshape([3, 3])
+        ori_com = np.array(
+            pybullet.getMatrixFromQuaternion(com_ori)
+        ).reshape([3, 3])
+        ori = np.dot(ori_joint, ori_com)
+        a = 0.05
+        offset_x = np.dot(ori_joint, np.array([a, 0, 0]))
+        offset_y = np.dot(ori_joint, np.array([0, a, 0]))
+        offset_z = np.dot(ori_joint, np.array([0, 0, a]))
+        print("SPH position: {}".format(np.array(joint)))
+        for i, offset in enumerate([offset_x, offset_y, offset_z]):
+            color = np.zeros(3)
+            color[i] = 1
+            pybullet.addUserDebugLine(
+                joint,
+                joint + offset,
+                lineColorRGB=color,
+                lineWidth=5,
+                lifeTime=1,
+            )
+            # com = data_gps.com_position(iteration, link_i)
+            # pybullet.addUserDebugLine(
+            #     com,
+            #     com + np.array([0, 0, 0.1]),
+            #     lineColorRGB=[0, 0, 1],
+            #     lifeTime=1
+            # )
