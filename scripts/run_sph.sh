c19c557f934aa299de292aa8b2bf51bc3ffae70e
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-07-17 09:39:26 +0200

    Updated script to run SPH simulation with GPU

--- scripts/run_sph.sh
+++ scripts/run_sph.sh
@@ -1,2 +1,2 @@
-python3 sph.py --pfreq 10 --openmp  # --openmp --openl --cuda
-# python3 sph.py --pfreq 10 --cuda  # --openmp --openl --cuda
+# python3 sph.py --pfreq 20 --openmp  # --openmp --openl --cuda
+python3 sph.py --pfreq 20 --cuda
