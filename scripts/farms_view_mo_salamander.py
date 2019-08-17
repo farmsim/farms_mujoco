97c71677b414d968d21419bbb656e070875fdeaa
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2019-08-17 18:29:04 +0200

    Added solution reordering of the reesults from salamander evolutiion

--- scripts/farms_view_mo_salamander.py
+++ scripts/farms_view_mo_salamander.py
@@ -1,60 +1,45 @@
 """Farms multiobjective optimisation for salamander"""
 
 import numpy as np
-
-from jmetal.core.problem import FloatProblem
 from jmetal.core.solution import FloatSolution
-# from jmetal.algorithm.multiobjective.moead import MOEAD
-from jmetal.algorithm.multiobjective.nsgaii import NSGAII
-from jmetal.operator import (
-    SBXCrossover,
-    PolynomialMutation,
-    DifferentialEvolutionCrossover
-)
-from jmetal.util.observer import ProgressBarObserver, VisualizerObserver
-from jmetal.util.solutions.evaluator import MultiprocessEvaluator
-from jmetal.util.termination_criterion import StoppingByEvaluations
-# from jmetal.util.aggregative_function import Tschebycheff
-from jmetal.util.solutions import (
-    # read_solutions,
-    print_function_values_to_file,
-    print_variables_to_file
-)
 from jmetal.util.ranking import FastNonDominatedRanking
-from jmetal.lab.visualization import Plot, InteractivePlot
-
-from farms_bullet.simulations.simulation_options import SimulationOptions
-from farms_bullet.experiments.salamander.simulation import SalamanderSimulation
-from farms_bullet.experiments.salamander.animat_options import SalamanderOptions
-
 from farms_mo_salamander import SalamanderEvolution
 
 
-def read_solutions(var_filename, fun_filename):
+def read_solutions(var_filename, fun_filename, order=0):
     """ Reads a reference front from a file.
 
     :param filename: File path where the front is located.
     """
+    variables = []
+    objectives = []
     front = []
     number_of_objectives = 2
 
+    # Variables
     with open(var_filename) as file:
         for line in file:
-            vector = [float(x) for x in line.split()]
-            solution = FloatSolution([], [], number_of_objectives)
-            solution.variables = vector
-            front.append(solution)
+            variables.append([float(x) for x in line.split()])
     with open(fun_filename) as file:
-        for i, line in enumerate(file):
-            vector = [float(x) for x in line.split()]
-            front[i].objectives = vector
+        for line in file:
+            objectives.append([float(x) for x in line.split()])
+    variables, objectives = map(np.array, [variables, objectives])
+    order = list(np.argsort(objectives[:, 0]))
+    print(order)
+    for _variables, _objectives in zip(variables[order], objectives[order]):
+        solution = FloatSolution([], [], number_of_objectives)
+        solution.variables = _variables
+        solution.objectives = _objectives
+        front.append(solution)
     return front
 
 
-def run_solutions(solutions, problem=SalamanderEvolution):
+def run_solutions(solutions, problem_class=SalamanderEvolution):
     """Show simulations """
     for solution in solutions:
+        problem = problem_class()
         objectives = solution.objectives.copy()
+        print("Objectives: {}".format(objectives))
         solution2 = problem.evaluate(solution, evolution=False)
         same_objectives = all([
             obj1 == obj2
@@ -72,8 +57,6 @@ def main():
     """Main"""
 
     # Load results from file
-    # print_function_values_to_file(front, 'FUN.' + algorithm.label)
-    # print_variables_to_file(front, 'VAR.' + algorithm.label)
     front = read_solutions(
         "VAR.NSGAII.Salamander evolution",
         "FUN.NSGAII.Salamander evolution"
