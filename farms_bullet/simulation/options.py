4971aba683e2cdd571b317eb80c8517e5f26c99a
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-04-22 13:44:18 +0200

    [REFACTOR] Removed farms_sdf and farms_data dependencies

--- farms_bullet/simulation/options.py
+++ farms_bullet/simulation/options.py
@@ -1,8 +1,134 @@
 """Simulation options"""
 
-from farms_data.options import Options
-from farms_sdf.units import SimulationUnitScaling
+import yaml
 from .parse_args import parse_args
+
+
+class Options(dict):
+    """Options"""
+
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+    def __getstate__(self):
+        """Get state"""
+        return self
+
+    def __setstate__(self, value):
+        """Get state"""
+        for item in value:
+            self[item] = value[item]
+
+    def to_dict(self):
+        """To dictionary"""
+        return {
+            key: value.to_dict() if isinstance(value, Options) else value
+            for key, value in self.items()
+        }
+
+    @classmethod
+    def load(cls, filename):
+        """Load from file"""
+        with open(filename, 'r') as yaml_file:
+            options = yaml.full_load(yaml_file)
+        return cls(**options)
+
+    def save(self, filename):
+        """Save to file"""
+        with open(filename, 'w+') as yaml_file:
+            yaml.dump(
+                self.to_dict(),
+                yaml_file,
+                default_flow_style=False
+            )
+
+
+class SimulationUnitScaling(Options):
+    """Simulation scaling
+
+    1 [m] in reality = self.meterss [m] in simulation
+    1 [s] in reality = self.seconds [s] in simulation
+    1 [kg] in reality = self.kilograms [kg] in simulation
+
+    """
+
+    def __init__(self, meters=1, seconds=1, kilograms=1):
+        super(SimulationUnitScaling, self).__init__()
+        self.meters = meters
+        self.seconds = seconds
+        self.kilograms = kilograms
+
+    @property
+    def hertz(self):
+        """Hertz (frequency)
+
+        Scaled as self.hertz = 1/self.seconds
+
+        """
+        return 1./self.seconds
+
+    @property
+    def newtons(self):
+        """Newtons
+
+        Scaled as self.newtons = self.kilograms*self.meters/self.time**2
+
+        """
+        return self.kilograms*self.acceleration
+
+    @property
+    def torques(self):
+        """Torques
+
+        Scaled as self.torques = self.kilograms*self.meters**2/self.time**2
+
+        """
+        return self.newtons*self.meters
+
+    @property
+    def velocity(self):
+        """Velocity
+
+        Scaled as self.velocities = self.meters/self.seconds
+
+        """
+        return self.meters/self.seconds
+
+    @property
+    def acceleration(self):
+        """Acceleration
+
+        Scaled as self.gravity = self.meters/self.seconds**2
+
+        """
+        return self.velocity/self.seconds
+
+    @property
+    def gravity(self):
+        """Gravity
+
+        Scaled as self.gravity = self.meters/self.seconds**2
+
+        """
+        return self.acceleration
+
+    @property
+    def volume(self):
+        """Volume
+
+        Scaled as self.volume = self.meters**3
+
+        """
+        return self.meters**3
+
+    @property
+    def density(self):
+        """Density
+
+        Scaled as self.density = self.kilograms/self.meters**3
+
+        """
+        return self.kilograms/self.volume
 
 
 class SimulationOptions(Options):
