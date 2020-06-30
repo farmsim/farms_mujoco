088ace162d6a958d69e5a6dbb0fc28b57ddd6704
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-06-30 11:48:40 +0200

    [SIMULATION] [OPTIONS] Code cleaning

--- farms_bullet/simulation/options.py
+++ farms_bullet/simulation/options.py
@@ -20,23 +20,31 @@ class SimulationOptions(Options):
             seconds=kwargs.pop('seconds', 1),
             kilograms=kwargs.pop('kilograms', 1)
         )
+
+        # Simulation
         self.timestep = kwargs.pop('timestep', 1e-3)
         self.n_iterations = kwargs.pop('n_iterations', 100)
-        self.n_solver_iters = kwargs.pop('n_solver_iters', 50)
-        self.free_camera = kwargs.pop('free_camera', False)
-        self.rotating_camera = kwargs.pop('rotating_camera', False)
-        self.top_camera = kwargs.pop('top_camera', False)
         self.play = kwargs.pop('play', True)
         self.fast = kwargs.pop('fast', False)
+        self.headless = kwargs.pop('headless', False)
+
+        # Camera
+        self.free_camera = kwargs.pop('free_camera', False)
+        self.top_camera = kwargs.pop('top_camera', False)
+        self.rotating_camera = kwargs.pop('rotating_camera', False)
+
+        # Video recording
         self.record = kwargs.pop('record', False)
         self.fps = kwargs.pop('fps', False)
         self.video_name = kwargs.pop('video_name', 'video')
         self.video_yaw = kwargs.pop('video_yaw', 0)
         self.video_pitch = kwargs.pop('video_pitch', -45)
         self.video_distance = kwargs.pop('video_distance', 1)
-        self.headless = kwargs.pop('headless', False)
-        self.opengl2 = kwargs.pop('opengl2', False)
+
+        # Pybullet
         self.gravity = kwargs.pop('gravity', [0, 0, -9.81])
+        self.opengl2 = kwargs.pop('opengl2', False)
+        self.n_solver_iters = kwargs.pop('n_solver_iters', 50)
         assert not kwargs, kwargs
 
     def duration(self):
@@ -49,20 +57,27 @@ class SimulationOptions(Options):
         clargs = parse_args()
         timestep = kwargs.pop('timestep', clargs.timestep)
         return cls(
+            # Simulation
             timestep=timestep,
             n_iterations=kwargs.pop('n_iterations', int(clargs.duration/timestep)),
-            n_solver_iters=kwargs.pop('n_solver_iters', clargs.n_solver_iters),
-            free_camera=kwargs.pop('free_camera', clargs.free_camera),
-            rotating_camera=kwargs.pop('rotating_camera', clargs.rotating_camera),
-            top_camera=kwargs.pop('top_camera', clargs.top_camera),
             play=kwargs.pop('play', not clargs.pause),
             fast=kwargs.pop('fast', clargs.fast),
+            headless=kwargs.pop('headless', clargs.headless),
+
+            # Camera
+            free_camera=kwargs.pop('free_camera', clargs.free_camera),
+            top_camera=kwargs.pop('top_camera', clargs.top_camera),
+            rotating_camera=kwargs.pop('rotating_camera', clargs.rotating_camera),
+
+            # Video recording
             record=kwargs.pop('record', clargs.record),
             fps=kwargs.pop('fps', clargs.fps),
             video_yaw=kwargs.pop('video_yaw', clargs.video_yaw),
             video_pitch=kwargs.pop('video_pitch', clargs.video_pitch),
             video_distance=kwargs.pop('video_distance', clargs.video_distance),
-            headless=kwargs.pop('headless', clargs.headless),
+
+            # Pybullet
             opengl2=kwargs.pop('opengl2', clargs.opengl2),
+            n_solver_iters=kwargs.pop('n_solver_iters', clargs.n_solver_iters),
             **kwargs,
         )
