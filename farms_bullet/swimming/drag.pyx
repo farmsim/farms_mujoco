c22c37e98effe075ecfe179be2e4196828ea53ed
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-10-06 16:36:57 +0200

    [SWIMMING] Cythonised swimming_debug

--- farms_bullet/swimming/drag.pyx
+++ farms_bullet/swimming/drag.pyx
@@ -1,10 +1,11 @@
 """Drag forces"""
 
-from farms_data.sensors.data_cy cimport HydrodynamicsArrayCy, GpsArrayCy
-
 import pybullet
 import numpy as np
 cimport numpy as np
+
+from farms_data.sensors.data_cy cimport HydrodynamicsArrayCy, GpsArrayCy
+import farms_pylog as pylog
 
 
 cdef np.ndarray compute_buoyancy(link, position, global2com, mass, surface, gravity):
@@ -216,3 +217,34 @@ cpdef void swimming_motion(
             torqueObj=np.array(hydro_torque),
             flags=flags,
         )
+
+
+cpdef swimming_debug(iteration, data_gps, links):
+    """Swimming debug"""
+    for link in links:
+        sensor_i = data_gps.index(link.name)
+        joint = np.array(data_gps.urdf_position(iteration, sensor_i))
+        joint_ori = np.array(data_gps.urdf_orientation(iteration, sensor_i))
+        # com_ori = np.array(data_gps.com_orientation(iteration, sensor_i))
+        ori_joint = np.array(
+            pybullet.getMatrixFromQuaternion(joint_ori)
+        ).reshape([3, 3])
+        # ori_com = np.array(
+        #     pybullet.getMatrixFromQuaternion(com_ori)
+        # ).reshape([3, 3])
+        # ori = np.dot(ori_joint, ori_com)
+        axis = 0.05
+        offset_x = np.dot(ori_joint, np.array([axis, 0, 0]))
+        offset_y = np.dot(ori_joint, np.array([0, axis, 0]))
+        offset_z = np.dot(ori_joint, np.array([0, 0, axis]))
+        pylog.debug('SPH position: {}'.format(np.array(joint)))
+        for i, offset in enumerate([offset_x, offset_y, offset_z]):
+            color = np.zeros(3)
+            color[i] = 1
+            pybullet.addUserDebugLine(
+                joint,
+                joint + offset,
+                lineColorRGB=color,
+                lineWidth=5,
+                lifeTime=1,
+            )
