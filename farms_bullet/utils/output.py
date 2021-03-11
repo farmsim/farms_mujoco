5aeea598c61be67bb9b9d3136f0de4eb570338ef
Jonathan Arreguit <jarreguit@gmail.com>
2021-03-11 16:58:13 +0100

    [REDIRECT] Avoid output redirection on Windows

--- farms_bullet/utils/output.py
+++ farms_bullet/utils/output.py
@@ -9,31 +9,34 @@ from inspect import currentframe, getframeinfo
 @contextlib.contextmanager
 def redirect_output(stdout_function=None):
     """Redirect output"""
-    stdout_fileno = sys.stdout.fileno()
-    stdout_original = os.dup(stdout_fileno)
-    stdout_pipe_read, stdout_pipe_write = os.pipe()
-    os.dup2(stdout_pipe_write, stdout_fileno)
-    os.close(stdout_pipe_write)
-    try:
+    if os.name == 'nt':
         yield
-    finally:
-        os.close(stdout_fileno)
-        if stdout_function is not None:
-            captured_stdout = ''
-            while True:
-                data = os.read(stdout_pipe_read, 256)
-                if not data:
-                    break
-                captured_stdout += data.decode('utf-8')
-        os.close(stdout_pipe_read)
-        os.dup2(stdout_original, stdout_fileno)
-        os.close(stdout_original)
-        if stdout_function is not None:
-            frame = currentframe().f_back.f_back
-            header = '[REDIRECT] {}::{}::{}():\n>   '.format(
-                getframeinfo(frame).filename,
-                frame.f_code.co_name,
-                frame.f_lineno,
-            )
-            captured_stdout = header + captured_stdout.replace('\n', '\n>   ')
-            stdout_function(captured_stdout)
+    else:
+        stdout_fileno = sys.stdout.fileno()
+        stdout_original = os.dup(stdout_fileno)
+        stdout_pipe_read, stdout_pipe_write = os.pipe()
+        os.dup2(stdout_pipe_write, stdout_fileno)
+        os.close(stdout_pipe_write)
+        try:
+            yield
+        finally:
+            os.close(stdout_fileno)
+            if stdout_function is not None:
+                captured_stdout = ''
+                while True:
+                    data = os.read(stdout_pipe_read, 256)
+                    if not data:
+                        break
+                    captured_stdout += data.decode('utf-8')
+            os.close(stdout_pipe_read)
+            os.dup2(stdout_original, stdout_fileno)
+            os.close(stdout_original)
+            if stdout_function is not None:
+                frame = currentframe().f_back.f_back
+                header = '[REDIRECT] {}::{}::{}():\n>   '.format(
+                    getframeinfo(frame).filename,
+                    frame.f_code.co_name,
+                    frame.f_lineno,
+                )
+                captured_stdout = header + captured_stdout.replace('\n', '\n>   ')
+                stdout_function(captured_stdout)
