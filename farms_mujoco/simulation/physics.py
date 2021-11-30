ba9b94e5f872a7c1a505e9649490dd42d38b9b85
Jonathan Arreguit <jarreguit@gmail.com>
2021-11-30 09:27:42 +0100

    [PHYSICS] Major improvement to physics handling and data transfer

--- farms_mujoco/simulation/physics.py
+++ farms_mujoco/simulation/physics.py
@@ -16,6 +16,8 @@ def links_data(physics, sensor_maps):
         ].reshape(
             [len(sensor_maps[identifier]['names']), n_c]
         )
+        if len(sensor_maps[identifier]['names']) > 0
+        else []
         for identifier, n_c in [
                 ['framepos', 3],
                 ['framequat', 4],
@@ -35,13 +37,28 @@ def joints_data(physics, sensor_maps):
             if n_c > 1
             else len(sensor_maps[identifier]['names'])
         )
+        if len(sensor_maps[identifier]['names']) > 0
+        else []
         for identifier, n_c in [
                 ['jointpos', 1],
                 ['jointvel', 1],
                 ['actuatorfrc_position', 1],
                 ['actuatorfrc_velocity', 1],
+                ['actuatorfrc_torque', 1],
         ]
     ]
+
+
+def row2index(row, name, single=False):
+    """Row to index"""
+    identifier = row.convert_key_item(name)
+    if isinstance(identifier, slice):
+        indices = [
+            np.arange(id_slice.start, id_slice.stop, id_slice.step)
+            for id_slice in [identifier]
+        ][0]
+        return indices[0] if single else indices
+    return identifier
 
 
 def get_sensor_maps(physics, verbose=True):
@@ -50,10 +67,14 @@ def get_sensor_maps(physics, verbose=True):
     sensors_names = sensors_row.names
     pylog.info('Sensors data:\n%s', physics.named.data.sensordata)
     sensors = [
-        'framepos', 'framequat', 'framelinvel', 'frameangvel',  # Links
-        'jointpos', 'jointvel',  # Joints
-        'actuatorfrc_position', 'actuatorfrc_velocity',  # Joints control
-        'touch',  # Contacts
+        # Links
+        'framepos', 'framequat', 'framelinvel', 'frameangvel',
+        # Joints
+        'jointpos', 'jointvel',
+        # Joints control
+        'actuatorfrc_position', 'actuatorfrc_velocity', 'actuatorfrc_torque',
+        # Contacts
+        'touch',
     ]
     sensor_maps = {
         sensor: {
@@ -67,13 +88,15 @@ def get_sensor_maps(physics, verbose=True):
     }
     for sensor_info in sensor_maps.values():
         sensor_info['indices'] = np.array([
+            row2index(row=sensors_row, name=name)
+            for name in sensor_info['names']
+        ])
+
+    for sensor_info in sensor_maps.values():
+        sensor_info['indices'] = np.array([
             [
-                np.arange(
-                    indices_slice.start,
-                    indices_slice.stop,
-                    indices_slice.step,
-                )
-                for indices_slice in [sensors_row.convert_key_item(name)]
+                np.arange(id_slice.start, id_slice.stop, id_slice.step)
+                for id_slice in [sensors_row.convert_key_item(name)]
             ][0]
             for name in sensor_info['names']
         ])
@@ -109,6 +132,7 @@ def get_sensor_maps(physics, verbose=True):
                     ['velocities', 'jointvel'],
                     ['torques (position)', 'actuatorfrc_position'],
                     ['torques (velocity)', 'actuatorfrc_velocity'],
+                    ['torques (torque)', 'actuatorfrc_torque'],
                 ],
                 joints_data(physics, sensor_maps),
         ):
@@ -145,7 +169,7 @@ def get_sensor_maps(physics, verbose=True):
             )
 
         # External forces in world frame
-        physics.data.xfrc_applied[:] = 0
+        # physics.data.xfrc_applied[:] = 0
         pylog.info(physics.named.data.xfrc_applied)
 
     return sensor_maps
@@ -159,28 +183,38 @@ def get_physics2data_maps(physics, sensor_data, sensor_maps):
     joints_names = sensor_data.joints.names
 
     # Links from physics
-    xpos_names = physics.named.data.xpos.axes.row.names
+    xpos_row = physics.named.data.xpos.axes.row
     sensor_maps['xpos2data'] = np.array([
-        xpos_names.index(link_name)
+        row2index(row=xpos_row, name=link_name)
         for link_name in links_names
     ])
-    xquat_names = physics.named.data.xquat.axes.row.names
+    xquat_row = physics.named.data.xquat.axes.row
     sensor_maps['xquat2data'] = np.array([
-        xquat_names.index(link_name)
+        row2index(row=xquat_row, name=link_name)
+        for link_name in links_names
+    ])
+    xipos_row = physics.named.data.xipos.axes.row
+    sensor_maps['xipos2data'] = np.array([
+        row2index(row=xipos_row, name=link_name)
+        for link_name in links_names
+    ])
+    cvel_row = physics.named.data.cvel.axes.row
+    sensor_maps['cvel2data'] = np.array([
+        row2index(row=cvel_row, name=link_name)
         for link_name in links_names
     ])
 
-    # # Joints from physics
-    # qpos_names = physics.named.data.xpos.axes.row.names
-    # sensor_maps['qpos2data'] = np.array([
-    #     qpos_names.index(joint_name)
-    #     for joint_name in joints_names
-    # ])
-    # qvel_names = physics.named.data.qvel.axes.row.names
-    # sensor_maps['qvel2data'] = np.array([
-    #     qvel_names.index(joint_name)
-    #     for joint_name in joints_names
-    # ])
+    # Joints from physics
+    qpos_row = physics.named.data.qpos.axes.row
+    sensor_maps['qpos2data'] = np.array([
+        row2index(row=qpos_row, name=joint_name, single=True)
+        for joint_name in joints_names
+    ])
+    qvel_row = physics.named.data.qvel.axes.row
+    sensor_maps['qvel2data'] = np.array([
+        row2index(row=qvel_row, name=joint_name, single=True)
+        for joint_name in joints_names
+    ])
 
     # Links - sensors
     for identifier in ['framepos', 'framequat', 'framelinvel', 'frameangvel']:
@@ -191,15 +225,19 @@ def get_physics2data_maps(physics, sensor_data, sensor_maps):
                 )
             ]
             for link_name in links_names
-        ])
-    sensor_maps['framequat2data'][:, :] = (
-        sensor_maps['framequat2data'][:, [1, 2, 3, 0]]
-    )
+        ]) if all(
+            f'{identifier}_{link_name}' in sensor_maps[identifier]['names']
+            for link_name in links_names
+        ) else []
+    if len(sensor_maps['framequat2data']) > 0:
+        sensor_maps['framequat2data'][:, :] = (
+            sensor_maps['framequat2data'][:, [1, 2, 3, 0]]
+        )
 
     # Joints - sensors
     for identifier in [
             'jointpos', 'jointvel',
-            'actuatorfrc_position', 'actuatorfrc_velocity',
+            'actuatorfrc_position', 'actuatorfrc_velocity', 'actuatorfrc_torque',
     ]:
         sensor_maps[f'{identifier}2data'] = np.array([
             sensor_maps[identifier]['indices'][
@@ -208,10 +246,13 @@ def get_physics2data_maps(physics, sensor_data, sensor_maps):
                 )
             ][0]
             for joint_name in joints_names
-        ])
+        ]) if all(
+            f'{identifier}_{joint_name}' in sensor_maps[identifier]['names']
+            for joint_name in joints_names
+        ) else []
 
     # Contacts
-    data_names = sensor_data.contacts.names[:4]
+    data_names = sensor_data.contacts.names
     body_names = physics.named.model.body_pos.axes.row.names
     sensor_maps['geom2data'] = {
         geom_id: data_names.index(body_names[body_id])
@@ -220,19 +261,47 @@ def get_physics2data_maps(physics, sensor_data, sensor_maps):
     }
     sensor_maps['geom_set'] = set(sensor_maps['geom2data'].keys())
 
+    # External forces
+    row = physics.named.data.xfrc_applied.axes.row
+    # hydro_names = sensor_data.hydrodynamics.names
+    # sensor_maps['data2xfrc'] = np.array([
+    #     hydro_names.index(name)
+    #     for name in row.names
+    # ])
+    sensor_maps['data2xfrc2'] = np.array([
+        row2index(row=row, name=name, single=True)
+        for name in sensor_data.hydrodynamics.names
+    ])
+    sensor_maps['datalinks2xfrc2'] = np.array([
+        row2index(row=row, name=name, single=True)
+        for name in sensor_data.links.names
+    ])
 
-def physics2data(physics, iteration, data, maps, units):
+
+def physicslinkssensors2data(physics, iteration, data, sensor_maps, units):
     """Sensors data collection"""
+    data.sensors.links.array[iteration, :,
+        sc.link_urdf_position_x:sc.link_urdf_position_z+1,
+    ] = physics.data.sensordata[sensor_maps['framepos2data']]
+    data.sensors.links.array[iteration, :,
+        sc.link_urdf_orientation_x:sc.link_urdf_orientation_w+1,
+    ] = physics.data.sensordata[sensor_maps['framequat2data']]
 
-    sensor_maps = maps['sensors']
 
-    # Links
-    # data.sensors.links.array[iteration, :,
-    #     sc.link_urdf_position_x:sc.link_urdf_position_z+1,
-    # ] = physics.data.sensordata[sensor_maps['framepos2data']]
-    # data.sensors.links.array[iteration, :,
-    #     sc.link_urdf_orientation_x:sc.link_urdf_orientation_w+1,
-    # ] = physics.data.sensordata[sensor_maps['framequat2data']]
+def physicslinksvelsensors2data(physics, iteration, data, sensor_maps, units):
+    """Sensors data collection"""
+    data.sensors.links.array[iteration, :,
+        sc.link_com_velocity_lin_x:sc.link_com_velocity_lin_z+1,
+    ] = physics.data.sensordata[sensor_maps['framelinvel2data']]/units.velocity
+    data.sensors.links.array[iteration, :,
+        sc.link_com_velocity_ang_x:sc.link_com_velocity_ang_z+1,
+    ] = (
+        physics.data.sensordata[sensor_maps['frameangvel2data']]
+    )/units.angular_velocity
+
+
+def physicslinks2data(physics, iteration, data, sensor_maps, units):
+    """Sensors data collection"""
     data.sensors.links.array[iteration, :,
         sc.link_urdf_position_x:sc.link_urdf_position_z+1,
     ] = physics.data.xpos[sensor_maps['xpos2data']]/units.meters
@@ -240,33 +309,67 @@ def physics2data(physics, iteration, data, maps, units):
         sc.link_urdf_orientation_x:sc.link_urdf_orientation_w+1,
     ] = physics.data.xquat[sensor_maps['xquat2data']][:, [1, 2, 3, 0]]
     data.sensors.links.array[iteration, :,
-            sc.link_com_velocity_lin_x:sc.link_com_velocity_lin_z+1,
-    ] = physics.data.sensordata[sensor_maps['framelinvel2data']]/units.velocity
+        sc.link_com_position_x:sc.link_com_position_z+1,
+    ] = physics.data.xipos[sensor_maps['xipos2data']]/units.meters
     data.sensors.links.array[iteration, :,
-            sc.link_com_velocity_ang_x:sc.link_com_velocity_ang_z+1,
-    ] = (
-        physics.data.sensordata[sensor_maps['frameangvel2data']]
-    )/units.angular_velocity
+        sc.link_com_orientation_x:sc.link_com_orientation_w+1,
+    ] = physics.data.xquat[sensor_maps['xquat2data']][:, [1, 2, 3, 0]]
 
-    # Joints
+
+def physicslinksvel2data(physics, iteration, data, sensor_maps, units):
+    """Sensors data collection"""
+    data.sensors.links.array[iteration, :,
+        sc.link_com_velocity_lin_x:sc.link_com_velocity_lin_z+1,
+    ] = physics.data.cvel[sensor_maps['cvel2data'], 3:]/units.velocity
+    data.sensors.links.array[iteration, :,
+        sc.link_com_velocity_ang_x:sc.link_com_velocity_ang_z+1,
+    ] = physics.data.cvel[sensor_maps['cvel2data'], :3]/units.angular_velocity
+
+
+def physicsjointssensors2data(physics, iteration, data, sensor_maps, units):
+    """Sensors data collection"""
     data.sensors.joints.array[iteration, :, sc.joint_position] = (
         physics.data.sensordata[sensor_maps['jointpos2data']]
     )
     data.sensors.joints.array[iteration, :, sc.joint_velocity] = (
         physics.data.sensordata[sensor_maps['jointvel2data']]
     )/units.angular_velocity
-    # data.sensors.joints.array[iteration, :, sc.joint_position] = (
-    #     physics.data.qpos[sensor_maps['qpos2data']]
-    # )
-    # data.sensors.joints.array[iteration, :, sc.joint_velocity] = (
-    #     physics.data.qvel[sensor_maps['qvel2data']]
-    # )
-    data.sensors.joints.array[iteration, :, sc.joint_torque] = (
-        physics.data.sensordata[sensor_maps['actuatorfrc_position2data']]
-        + physics.data.sensordata[sensor_maps['actuatorfrc_velocity2data']]
-    )/units.torques
 
-    # Contacts
+
+def physicsjoints2data(physics, iteration, data, sensor_maps, units):
+    """Sensors data collection"""
+    data.sensors.joints.array[iteration, :, sc.joint_position] = (
+        physics.data.qpos[sensor_maps['qpos2data']]
+    )
+    data.sensors.joints.array[iteration, :, sc.joint_velocity] = (
+        physics.data.qvel[sensor_maps['qvel2data']]
+    )/units.angular_velocity
+
+
+def physicsactuators2data(physics, iteration, data, sensor_maps, units):
+    """Sensors data collection"""
+    itorques = 1./units.torques
+    if len(sensor_maps['actuatorfrc_position2data']) > 0:
+        data.sensors.joints.array[iteration, :, sc.joint_torque] += (
+            physics.data.sensordata[sensor_maps['actuatorfrc_position2data']]
+        )*itorques
+    if len(sensor_maps['actuatorfrc_velocity2data']) > 0:
+        data.sensors.joints.array[iteration, :, sc.joint_torque] += (
+            physics.data.sensordata[sensor_maps['actuatorfrc_velocity2data']]
+        )*itorques
+    if len(sensor_maps['actuatorfrc_torque2data']) > 0:
+        data.sensors.joints.array[iteration, :, sc.joint_torque] += (
+            physics.data.sensordata[sensor_maps['actuatorfrc_torque2data']]
+        )*itorques
+
+
+def physics2data(physics, iteration, data, maps, units):
+    """Sensors data collection"""
+    sensor_maps = maps['sensors']
+    physicslinks2data(physics, iteration, data, sensor_maps, units)
+    physicslinksvelsensors2data(physics, iteration, data, sensor_maps, units)
+    physicsjoints2data(physics, iteration, data, sensor_maps, units)
+    physicsactuators2data(physics, iteration, data, sensor_maps, units)
     cycontacts2data(
         physics=physics,
         iteration=iteration,
